59f4b24068977a461f80c7a9e487ca36fb26e041
mikels0n <myskamichal@gmail.com>
2024-04-14 11:50:06 +0200

    Working buttons with numbers, vscode plugin for preview added

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 let numberOne = 0;
 let numberTwo = 0;
 let operator = '';
+let numberOnDisplay = [];
+const one = document.querySelector("#one");
+const two = document.querySelector("#two");
+const three = document.querySelector("#three");
+const four = document.querySelector("#four");
+const five = document.querySelector("#five");
+const six = document.querySelector("#six");
+const seven = document.querySelector("#seven");
+const eight = document.querySelector("#eight");
+const nine = document.querySelector("#nine");
+const zero = document.querySelector("#zero");
+const display = document.querySelector('#display');
+const clear = document.querySelector('#clear')
 
 function add(numberOne,numberTwo) {
     numberOne += numberTwo;
@@ -21,6 +34,75 @@ function divide(numberOne,numberTwo) {
     numberOne = numberOne / numberTwo;
     return numberOne;
 }
+
+function arrayToNum(array) {
+    let num = array.toString();
+    num = num.replaceAll(",","");
+    return(num);
+}
+
+function refreshDisplay() {
+    display.textContent = arrayToNum(numberOnDisplay);
+}
+
+function clearArray(array) {
+}
+
+one.addEventListener('click', () => {
+    numberOnDisplay.push("1");
+    refreshDisplay();
+})
+
+two.addEventListener('click', () => {
+    numberOnDisplay.push("2");
+    refreshDisplay();
+})
+
+three.addEventListener('click', () => {
+    numberOnDisplay.push("3");
+    refreshDisplay();
+})
+
+four.addEventListener('click', () => {
+    numberOnDisplay.push("4");
+    refreshDisplay();
+})
+
+five.addEventListener('click', () => {
+    numberOnDisplay.push("5");
+    refreshDisplay();
+})
+
+six.addEventListener('click', () => {
+    numberOnDisplay.push("6");
+    refreshDisplay();
+})
+
+seven.addEventListener('click', () => {
+    numberOnDisplay.push("7");
+    refreshDisplay();
+})
+
+eight.addEventListener('click', () => {
+    numberOnDisplay.push("8");
+    refreshDisplay();
+})
+
+nine.addEventListener('click', () => {
+    numberOnDisplay.push("9");
+    refreshDisplay();
+})
+
+zero.addEventListener('click', () => {
+    numberOnDisplay.push("0");
+    refreshDisplay();
+})
+
+clear.addEventListener('click', () => {
+    numberOnDisplay = [];
+    numberOnDisplay.push("0");
+    refreshDisplay;
+})
 
 function operate(operator, numberOne, numberTwo) {
     switch (operator) {
