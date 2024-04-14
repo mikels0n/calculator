469e48d5ee79626ff1e9deaae21a9afe36713adc
mikels0n <myskamichal@gmail.com>
2024-04-14 12:04:18 +0200

    clear button working

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let numberOne = 0;
 let numberTwo = 0;
 let operator = '';
-let numberOnDisplay = [];
+let numberOnDisplay = ["0"];
 const one = document.querySelector("#one");
 const two = document.querySelector("#two");
 const three = document.querySelector("#three");
@@ -45,63 +45,85 @@ function refreshDisplay() {
     display.textContent = arrayToNum(numberOnDisplay);
 }
 
+refreshDisplay();
+
 function clearArray(array) {
+    let x = array.length;
+    for(let i = 0; i < x; i++) {
+        array.shift();
+    }
+    numberOnDisplay.push("0");
+    refreshDisplay();
 }
 
+function checkZeroOnDisplay() {
+    if (numberOnDisplay[0] == "0") {
+        numberOnDisplay.shift();
+    }
+} 
+
 one.addEventListener('click', () => {
+    checkZeroOnDisplay();
     numberOnDisplay.push("1");
     refreshDisplay();
 })
 
 two.addEventListener('click', () => {
+    checkZeroOnDisplay();
     numberOnDisplay.push("2");
     refreshDisplay();
 })
 
 three.addEventListener('click', () => {
+    checkZeroOnDisplay();
     numberOnDisplay.push("3");
     refreshDisplay();
 })
 
 four.addEventListener('click', () => {
+    checkZeroOnDisplay();
     numberOnDisplay.push("4");
     refreshDisplay();
 })
 
 five.addEventListener('click', () => {
+    checkZeroOnDisplay();
     numberOnDisplay.push("5");
     refreshDisplay();
 })
 
 six.addEventListener('click', () => {
+    checkZeroOnDisplay();
     numberOnDisplay.push("6");
     refreshDisplay();
 })
 
 seven.addEventListener('click', () => {
+    checkZeroOnDisplay();
     numberOnDisplay.push("7");
     refreshDisplay();
 })
 
 eight.addEventListener('click', () => {
+    checkZeroOnDisplay();
     numberOnDisplay.push("8");
     refreshDisplay();
 })
 
 nine.addEventListener('click', () => {
+    checkZeroOnDisplay();
     numberOnDisplay.push("9");
     refreshDisplay();
 })
 
 zero.addEventListener('click', () => {
+    checkZeroOnDisplay();
     numberOnDisplay.push("0");
     refreshDisplay();
 })
 
 clear.addEventListener('click', () => {
-    numberOnDisplay = [];
-    numberOnDisplay.push("0");
-    refreshDisplay;
+    clearArray(numberOnDisplay);
 })
 
 function operate(operator, numberOne, numberTwo) {
